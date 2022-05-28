1f9ab45285ed145599cf416981cf550659b9f9e9
hrokr <alex.rook@gmail.com>
2022-05-28 10:13:37 -0400

    change: fixes refernece numbers, exports working data to preset folder

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 
 import pandas as pd
-from csv_processor import baseline_processing
+from csv_processor import baseline_processing, fix_references
 
 data_directory = os.path.join("../data", "RAW_ASAM.csv")
 df = pd.read_csv(data_directory, skipinitialspace = True)
@@ -10,5 +10,8 @@ df = pd.read_csv(data_directory, skipinitialspace = True)
 if __name__ == "__main__":
 
     baseline_processing(df)
+    fix_references(df)
     print(df)
 
+    os.makedirs('../data/in_progress', exist_ok=True)  
+    df.to_csv('../data/in_progress/v1_lat_long.csv') 
