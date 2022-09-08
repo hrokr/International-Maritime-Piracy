972510e08c7de4b92ab90d4203bcf18214069792
hrokr <alex.rook@gmail.com>
2022-09-07 22:14:36 -0400

    Add: docstrings

--- src/csv_processor.py
+++ src/csv_processor.py
@@ -1,17 +1,31 @@
-import pandas as pd
 import re
+
+import pandas as pd
 
 
 def preprocess(df):
+    """
+    preprocess csv by:
+        Drop columns with no event description (critial issue)
+        Drop columns where all value are NaN (i.e., 'Unnamed: 8')
+        Change NaN values to 'No Information' (critical issue)
+        Convert date column to datetime (used for index)
+    """
+
     df.dropna(subset=["description"], inplace=True)
     df.dropna(axis=1, how="all", inplace=True)
     df.fillna("No information", inplace=True)
-    df['date'] = pd.to_datetime(df['date'])
+    df["date"] = pd.to_datetime(df["date"])
 
     return df
 
 
-def splitLatLong(df: str) -> str:
+def splitLatLong(df):
+    """
+    Takes a single combined latitude/longitude position, breaks into two
+    columns, and gets rid of the old column.
+    """
+
     df = df.join(df["position"].str.split(expand=True)).rename(
         columns={0: "latitude", 1: "longitude"}
     )
@@ -20,7 +34,11 @@ def splitLatLong(df: str) -> str:
     return df
 
 
-def dms2dd(field) -> int:
+def dms2dd(field) -> float:
+    """
+    Converts position from Degrees, Minutes, Seconds (DMS) to decimal degrees.
+    """
+
     degrees, minutes, seconds, direction = re.split("[°'\"]+", field)
     dd = float(degrees) + float(minutes) / 60 + float(seconds) / (60 * 60)
     if direction in ("S", "W"):
@@ -30,6 +48,11 @@ def dms2dd(field) -> int:
 
 
 def conv_to_dd(df):
+    """
+    Sends latitude and longitude columns to dms2dd function for conversion to
+    decimal degrees.
+    """
+
     df["latitude"] = df["latitude"].apply(dms2dd)
     df["longitude"] = df["longitude"].apply(dms2dd)
 
@@ -40,12 +63,21 @@ def baseline_processing(df):
     preprocess(df)
     df = splitLatLong(df)
     df = conv_to_dd(df)
-    print("file cleaned")
+    print("Preprocessing completed")
 
     return df
 
+
 def fix_references(df):
-    df['reference'] = (df['date'].dt.year.astype(str) + '-' + (df.sort_values('date')
-                    .groupby(df['date'].dt.year).cumcount()+1).astype(str))
-    
+    """
+    Fixes inconsistent reference column numbering by ordering events by calendar year.
+    """
+
+    df["reference"] = (
+        df["date"].dt.year.astype(str)
+        + "-"
+        + (df.sort_values("date").groupby(df["date"].dt.year).cumcount() + 1).astype(str)
+    )
+    print("Reference column fixed")
+
     return df
