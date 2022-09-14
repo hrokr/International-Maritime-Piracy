ac88ff9f6787aec40487eafd678984f05e92c590
hrokr <alex.rook@gmail.com>
2022-09-14 00:31:51 -0400

    Change background on map to USGS sat

--- src/dash_app.py
+++ src/dash_app.py
@@ -6,12 +6,30 @@ import plotly_express as px
 df = pd.read_pickle("../data/in_progress/pickled_data.pkl")
 app = Dash(__name__)
 
-
+# world map element
 fig = px.scatter_mapbox(
     data_frame=df,
     lat="latitude",
     lon="longitude",
-    mapbox_style="carto-darkmatter",
+    mapbox_style="white-bg",
+    zoom=1,
     color_discrete_sequence=["gold"],
 )
+
+fig.update_layout(
+    mapbox_style="white-bg",
+    mapbox_layers=[
+        {
+            "below": "traces",
+            "sourcetype": "raster",
+            "sourceattribution": "United States Geological Survey",
+            "source": [
+                """https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/
+                MapServer/tile/{z}/{y}/{x}"""
+            ],
+        }
+    ],
+)
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
 fig.show()
