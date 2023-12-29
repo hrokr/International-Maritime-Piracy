ee87f6c45d3fe83375c8709279f78aed45b2563e
hrokr <alex.rook@gmail.com>
2023-12-28 22:46:31 -0500

    Add: Subregion tabs (not working); shows detail of indiv events

--- src/dashboard.py
+++ src/dashboard.py
@@ -6,13 +6,11 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 
 df = pd.read_pickle("../data/in_progress/pickled_data.pkl")
-
-# Extract year from date
 df["year"] = df["date"].dt.year
 
 app = dash.Dash(__name__)
 
-# Create a RangeSlider for years
+# RangeSlider for main map
 year_slider = dcc.RangeSlider(
     id="year-slider",
     min=df["year"].min(),
@@ -22,45 +20,24 @@ year_slider = dcc.RangeSlider(
     step=None,
 )
 
-# app.layout = html.Div([
-#     dcc.Graph(id='map-graph', style={'height': '90vh'}),  # Map takes the entire height of the viewport
-#     html.Div(year_slider, style={'margin': '10px'})
-# ])
-# app.layout = html.Div(
-#     [
-#         dcc.Graph(
-#             id="map-graph", style={"height": "90vh"}
-#         ),  # Map takes the entire height of the viewport
-#         html.Div(
-#             year_slider, style={"width": "100%", "margin": "10px"}
-#     ]
-app.layout = html.Div(
-    [
-        dcc.Graph(
-            id="map-graph", style={"height": "90vh"}
-        ),  # Map takes the entire height of the viewport
-        html.Div(year_slider, style={"margin": "10px"}),
-    ]
-)
-# )
+# Create map figures based on input data and options
+def create_map_figure(df, nav_areas, year_range=None):
+    filtered_df = df[df["navArea"].isin(nav_areas)]
 
-
-@app.callback(Output("map-graph", "figure"), [Input("year-slider", "value")])
-def update_figure(selected_years):
-    filtered_df = df[
-        (df["year"] >= selected_years[0]) & (df["year"] <= selected_years[1])
-    ]
-
+    if year_range:
+        filtered_df = filtered_df[
+            (filtered_df["year"] >= year_range[0])
+            & (filtered_df["year"] <= year_range[1])
+        ]
     fig = px.scatter_mapbox(
         data_frame=filtered_df,
         lat="latitude",
         lon="longitude",
         center={"lat": 10, "lon": 0},
         color_discrete_sequence=["gold"],
-        zoom=1.3,  # Show the entire world
+        zoom=1.3,
         height=600,
     )
-
     fig.update_layout(
         mapbox_style="white-bg",
         mapbox_layers=[
@@ -74,9 +51,75 @@ def update_figure(selected_years):
             }
         ],
         margin={"r": 0, "t": 0, "l": 0, "b": 0},
-    ),
-
+    )
     return fig
+
+
+# Tab layouts for regional maps
+tabs = dcc.Tabs(
+    id="map-tabs",
+    value="all",
+    children=[
+        dcc.Tab(label="All Areas", value="all"),
+        dcc.Tab(label="I, II, & III", value="123"),
+        dcc.Tab(label="VII & IX", value="79"),
+        dcc.Tab(label="XI", value="11"),
+        dcc.Tab(label="IV", value="4"),
+    ],
+)
+
+# Use grid-based layout
+app.layout = html.Div(
+    style={
+        "display": "flex",
+        "align-items": "flex-start",
+    },  # Apply flexbox to the main layout
+    children=[
+        html.Div(
+            [
+                tabs,
+                dcc.Graph(id="map-graph", style={"height": "60vh"}),
+                year_slider,
+            ],
+            style={"flex": "66%"},
+        ),
+        html.Div(
+            id="event-list",
+            style={"flex": "33%", "overflow-y": "scroll", "height": "60vh"},
+        ),
+    ],
+)
+
+
+## Callbacks
+
+# Update the map based on selected tab and year slider
+@app.callback(
+    Output("map-graph", "figure"),
+    [Input("map-tabs", "value"), Input("year-slider", "value")],
+)
+def update_map(selected_tab, selected_years):
+    if selected_tab == "all":
+        nav_areas = df["navArea"].unique()
+    else:
+        nav_areas = [int(area) for area in selected_tab]
+    return create_map_figure(df, nav_areas, year_range=selected_years)
+
+
+@app.callback(
+    Output("event-list", "children"),
+    [Input("map-graph", "clickData")],
+)
+def display_events(click_data):
+    if click_data:
+        point_index = click_data["points"][0]["pointIndex"]
+        description = df.iloc[point_index]["description"]
+        return html.Div(
+            description,
+            style={"overflow-y": "scroll", "height": "60vh"},  # Add scrollbar and height
+        )
+    else:
+        return html.Div(["Click on a point in the map to see its description"])
 
 
 if __name__ == "__main__":
